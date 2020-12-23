5f5b7ea15583214b4cbf00ee2e147cc699d26135
MagimbiJr <charleatanasi@gmail.com>
2020-12-23 04:44:24 +0300

    add bottom navigation

--- app/src/main/java/com/tana/foodapp/MainActivity.java
+++ app/src/main/java/com/tana/foodapp/MainActivity.java
@@ -1,14 +1,57 @@
 package com.tana.foodapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.tana.foodapp.Fragments.FavoriteFragment;
+import com.tana.foodapp.Fragments.HomeFragment;
+import com.tana.foodapp.Fragments.OrdersFragment;
+import com.tana.foodapp.Fragments.ProfileFragment;
+import com.tana.foodapp.Fragments.WalletFragment;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView mBottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBottomNav = findViewById(R.id.bottom_nav);
+        mBottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()) {
+                    case R.id.action_favorite:
+                        fragment = new FavoriteFragment();
+                        break;
+                    case R.id.action_home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.action_order:
+                        fragment = new OrdersFragment();
+                        break;
+                    case R.id.action_profile:
+                        fragment = new ProfileFragment();
+                        break;
+                    case R.id.action_wallet:
+                        fragment = new WalletFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+                return true;
+            }
+        });
+//        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+        if (savedInstanceState == null) {
+            mBottomNav.setSelectedItemId(R.id.action_home);
+        }
     }
 }
